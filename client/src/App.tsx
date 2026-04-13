ddb9bd873feb8d93357b27bd4d37ccc3a6b52892
earl-02 <ebering@filamer.edu.ph>
2026-04-13 21:10:51 +0800

    Icon-Layouts-Routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,10 @@
-import './App.css'
+import { RouterProvider } from "react-router-dom";
+import { Routes } from "./routes/Routes";
 
-function App() {
-  let name: string = "Earl";
-  let age: number = 20;
-  let score1: number = 85;
-  let score2: number = 90;
-  let totalScore: number = score1 + score2;
-  let average: number = totalScore / 2;
+const App = () => {
 
-  //Comment
+  return <RouterProvider router={Routes} />;
 
-  return(
-    <> {/* FRAGMENT*/}
-      <h1>Student Information</h1>
-      <p>Name: {name}</p>
-      <p>Age: {age}</p>
+};
 
-      <h2>Scores</h2>
-      <p>Score 1: {score1}</p>
-      <p>Score 2: {score2}</p>
-
-      <h3>Total Score: {totalScore}</h3>
-      <h3>Average Score: {average}</h3>
-
-    </>
-  )
-}
-
-export default App
+export default App;

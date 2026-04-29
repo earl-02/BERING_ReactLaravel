23cae7f5f9af65e1187c97b2f626f2dade8d00d6
earl-02 <ebering@filamer.edu.ph>
2026-04-29 14:55:48 +0800

    Part 4 Adjustments

--- client/src/hooks/useOnClickOutside.ts
+++ client/src/hooks/useOnClickOutside.ts
@@ -1,27 +1,30 @@
-import {useEffect, type RefObject} from 'react';
+import { useEffect, type RefObject } from 'react';
 
+// We use <T extends HTMLElement> to make it flexible
+// We use RefObject<T | null> because React refs start as null
+export const useOnClickOutside = <T extends HTMLElement>(
+  ref: RefObject<T | null>, 
+  handler: (event: MouseEvent | TouchEvent) => void
+) => {
+  useEffect(() => {
+    const listener = (event: MouseEvent | TouchEvent) => {
+      const el = ref?.current;
 
-export function useOnClickOutside<T extends HTMLElement>(
-    ref: RefObject<T | null>,
-    handler: (event: MouseEvent | TouchEvent) => void
-) {
-    useEffect(() => {
-        const listener = (event: MouseEvent | TouchEvent) => {
-            const el = ref?.current;
+      // Do nothing if clicking ref's element or descendent elements
+      // We also check if el exists to satisfy TS
+      if (!el || el.contains(event.target as Node)) {
+        return;
+      }
 
-            if (!el || el.contains(event.target as Node)) {
-                return;
-            }
+      handler(event);
+    };
 
-            handler(event);
-        };
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
 
-        document.addEventListener('mousedown', listener);
-        document.addEventListener('touchstart', listener);
-
-        return () => {
-            document.removeEventListener('mousedown', listener);
-            document.removeEventListener('touchstart', listener);
-        }
-    }, [ref, handler]);
-}
+    return () => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [ref, handler]);
+};
